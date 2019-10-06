eaf617c94ae2af9a6357825590c71a2e697bcd0b
Giuseppe Vadrucci <givadrucci@gmail.com>
2019-10-06 20:25:58 +0200

    Update vo.go

--- struct/vo.go
+++ struct/vo.go
@@ -5,7 +5,27 @@ type HealthCheckResponse struct {
 	Message string `json:"message"`
 }
 
-type Bann struct {
-	Name string `json:"name"`
-	Type string `json:"type"`
+type bann struct {
+	
+        languageCode int `json:"zoneId"`,
+        width int `json:"width"`,
+	height int `json:"height"`,
+	banners struct {
+		pageId int `json:"id"`,
+	        date string `json:"date"`,
+	        background_color string `json:"background_color"`,
+	        background_image int `json:"background_image"`,
+		button_text string `json:button_text"`,
+		button_color string `json:button_color"`,
+		button_background string `json:button_background`,
+		title string `json:title`,
+		button_text string `Play Now!"`,
+    
+        "description": "Play Now!",
+        "text_align": "left",
+        "link": "",
+        "link_isExternal": true
+      }
+    ]
+  },
 }

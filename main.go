6cdcc2a1c0902403d2ed9b53fa0a89bccc415621
Giuseppe Vadrucci <givadrucci@gmail.com>
2019-10-06 20:36:22 +0200

    Update main.go

--- main.go
+++ main.go
@@ -8,36 +8,13 @@ import (
 
 func main() {
 	e := echo.New()
-	type User struct {
-	Name  string `json:"name" xml:"name" form:"name" query:"name"`
-	Email string `json:"email" xml:"email" form:"email" query:"email"`
-}
-e.POST("/users", saveUser)
-e.GET("/users/:id", getUser)
-e.PUT("/users/:id", updateUser)
-e.DELETE("/users/:id", deleteUser)
-
-// e.GET("/users/:id", getUser)
-func getUser(c echo.Context) error {
-  	// User ID from path `users/:id`
-  	id := c.Param("id")
-	return c.String(http.StatusOK, id)
+	type Banners struct {
+	languageCode  string `json:"zoneId" xml:"zoneId" form:"languageCode" query:"languageCode"`
+	pageId string `json:"id" xml:"id" form:"id" query:"id"`
+	//others elements
 }
 
-//e.GET("/show", show)
-func show(c echo.Context) error {
-	// Get team and member from the query string
-	team := c.QueryParam("team")
-	member := c.QueryParam("member")
-	return c.String(http.StatusOK, "team:" + team + ", member:" + member)
-}
 
-e.POST("/users", func(c echo.Context) error {
-	u := new(User)
-	if err := c.Bind(u); err != nil {
-		return err
-	}
-	return c.JSON(http.StatusCreated, u)
-})
+
 	e.Logger.Fatal(e.Start(":1323"))
 }
